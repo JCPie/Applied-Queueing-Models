d1b5c72b04c8cb93684e08ccdce0e30ab91e5b32
LBMeelhuijsen <l.b.meelhuijsen@student.utwente.nl>
2024-07-08 22:15:39 +0200

    misschien klopt 4a? idk

--- AQM4/main.py
+++ AQM4/main.py
@@ -59,63 +59,183 @@ for i in range(var.n):
 
 print("P aggregated: \n", P_agg)
 
-Q = np.zeros([var.n + 1, var.n])
-Q[0, 0] = 1
-for i in range(1, var.n + 1):
-    Q[i, :] = arrival_rate_agg[i - 1] * P_agg[i-1, :]
-    for j in range(var.n):
-        Q[i,j] = Q[i,j] / arrival_rate_agg[j]
-
-print("Q: \n", Q)
-
-v = np.zeros(var.n)
+agg_rate_matrix = P_agg
 for i in range(var.n):
-    v[i] += 1 / np.sum(Q[:, i]**2)
+    agg_rate_matrix[i,:] = arrival_rate_agg[i] * agg_rate_matrix[i,:]
+print("Rate matrix: \n", agg_rate_matrix)
 
-print("v: \n", v)
-
-rho = np.zeros([var.n])
-for i in range(var.n):
-    rho[i] += arrival_rate_agg[i] * expected_service_time_agg[i] / (var.servers[i] * 60)
-
-print("rho: \n", rho)
-
-w = np.zeros(var.n)
-for i in range(var.n):
-    w[i] = 1 / (1 + 4 * ((1 - rho[i])**2) * (v[i] - 1))
-
-print("w: \n", w)
-
-x = np.zeros(var.n)
-for i in range(var.n):
-    x[i] += 1 + (var.servers[i]**(-0.5)) * (np.max([SCV_agg[i], 0.2]) - 1)
-
-print("x: \n", x)
-
-a = np.zeros(var.n)
+V = np.zeros(var.n)
 for j in range(var.n):
-    a[j] += 1 + w[j] * (Q[0,j] * SCV_agg[j] - 1)
-    for i in range(var.n):
-        a[j] += w[j] * (Q[i,j] * ((1 - P_agg[i,j]) + P_agg[i,j] * (rho[i]**2) * x[i]))
+    V[j] += np.sum(agg_rate_matrix[:,j]) / arrival_rate_agg[0]
 
-print("a: \n", a)
+print("V: \n", V)
 
-b_matrix = np.eye(var.n)
-for i in range(var.n):
-    for j in range(var.n):
-        b_matrix[i,j] += w[j] * P_agg[i,j] * Q[i,j] * (1 - (rho[i]**2))
+N = 25
+EW = np.zeros([N+1, var.n])
+Qc = np.zeros([N+1, var.n])
+ES_rem = np.zeros(var.n)
+for j in range(var.n):
+    ES_rem[j] += (((var.servers[j] - 1) / (var.servers[j] + 1)) * (expected_service_time_agg[j] / var.servers[j]) +
+                  (2 / (var.servers[j] + 1)) * (1 / var.servers[j]) * (
+                              expected_service_time_agg_sq[j] / (2 * expected_service_time_agg[j])))
 
-print("b_matrix: \n", b_matrix)
-
-SCV_arrival_agg = np.linalg.solve(b_matrix, a)
-
-print("SCV_arrival_agg: \n", SCV_arrival_agg)
-
-SCV_departure_agg = np.zeros([var.n])
-for i in range(var.n):
-    SCV_departure_agg[i] += (1 - (rho[i]**2)) * SCV_arrival_agg[i] + (rho[i]**2) * SCV_agg[i]
-
-print("SCV_departure_agg: \n", SCV_departure_agg)
-
+EL_Q = np.zeros([N+1, var.n])
+TH = np.zeros([N+1, var.n])
+p = np.zeros([var.n, N+1, N+1])
+for j in range(var.n):
+    p[j, 0, 0] += 1
 
 #Alg 5
+for n in range(1, N+1):
+    for j in range(var.n):
+        for k in range(int(np.min([var.servers[j], n])), n):
+            Qc[n-1, j] += p[j, k, n-1]
+
+        for k in range(int(np.min([var.servers[j] + 1, n])), n):
+            EL_Q[n-1, j] += (k - var.servers[j]) * p[j, k, n-1]
+
+        EW[n, j] += Qc[n-1, j] * ES_rem[j] + EL_Q[n-1, j] * (expected_service_time_agg[j] / var.servers[j]) + expected_service_time_agg[j]
+
+    soujourn = 0
+    for j in range(var.n):
+        soujourn += (V[j] * EW[n, j])
+
+    TH[n, 0] += n / soujourn
+
+    for j in range(1, var.n):
+        TH[n, j] += V[j] * TH[n, 0]
+
+    for j in range(var.n):
+        p[j, 0, n] = 1
+        for k in range(1,n+1):
+            p[j, k, n] = (expected_service_time_agg[j] / np.min([var.servers[j], k])) * TH[n, j] * p[j, k-1, n-1]
+            p[j, 0, n] -= p[j, k, n]
+
+print("EW: \n", EW)
+print("TH: \n", TH)
+# print("p: \n", p)
+print("ES_rem: \n", ES_rem)
+
+for i, rate in enumerate(TH[:, 0]):
+    if rate > arrival_rate_agg[0]:
+        print(f"Minimum required cards is: {i}")
+        break
+
+THC_M1 = TH[N, 0]
+print("TH^C_M+1 : \n", THC_M1)
+
+lambda_in = arrival_rate_agg[0]
+q = 1 - (lambda_in / THC_M1)
+print("q: \n", q)
+
+
+EW_LD = np.zeros([N+1, var.n])
+Qc_LD = np.zeros([N+1, var.n])
+EL_Q_LD = np.zeros([N+1, var.n])
+TH_LD = np.zeros([N+1, var.n])
+p_LD = np.zeros([var.n, N+1, N+1])
+for j in range(var.n):
+    p_LD[j, 0, 0] += 1
+
+for n in range(1, N+1):
+    EW_LD[n, 0] = (EL_Q_LD[n-1, 0] + Qc_LD[n-1, 0]) / lambda_in + p[0, 0, n-1] * q / lambda_in
+
+    for j in range(1, var.n):
+        EW_LD[n, j] += expected_service_time_agg[j]
+        for k in range(int(np.min([var.servers[j], n])), n):
+            EW_LD[n,j] += (k - var.servers[j] + 1) / (var.servers[j] / expected_service_time_agg[j]) * p[j, k, n-1]
+
+    soujourn = 0
+    for j in range(var.n):
+        soujourn += V[j] * EW_LD[n, j]
+    TH_LD[n, 0] += n / soujourn
+
+    for j in range(1, var.n):
+        TH_LD[n, j] += V[j] * TH_LD[n, 0]
+    for j in range(1, var.n):
+        p_LD[j, 0, n] = 1
+        for k in range(1,n+1):
+            p_LD[j, k, n] = (expected_service_time_agg[j] / np.min([var.servers[j], k])) * TH_LD[n, j] * p[j, k-1, n-1]
+            p_LD[j, 0, n] -= p[j, k, n]
+
+    Qc_LD[n, 0] = (TH_LD[n, 0] / lambda_in) * (Qc_LD[n-1, 0] + p[0, 0, n-1] * q)
+    p[0, 0, n] = 1 - Qc_LD[n, 0]
+    EL_Q_LD[n, 0] = TH_LD[n, 0] * EW_LD[n, 0]
+
+print("EW_LD: \n", EW_LD)
+print("TH_LD: \n", TH_LD)
+print(Qc_LD)
+
+##################  DISAGRAGATE ################
+rho = np.zeros(var.n)
+for i in range(var.n):
+    rho[i] = expected_service_time_agg[i] * arrival_rate_agg[i] / var.servers[i]
+print("rho: \n", rho)
+
+EL = np.zeros(var.n)
+for i in range(var.n):
+    EL[i] += arrival_rate_agg[i] * EW_LD[N, i]
+
+print("EL: \n", EL)
+
+EL_Q = EL
+for i in range (var.n):
+    if var.servers[i] == np.inf:
+        continue
+    EL_Q[i] -= var.servers[i] * rho[i]
+
+print("EL_Q: \n", EL_Q)
+
+EL_Q_b = np.zeros(var.n)
+for i in range(var.n):
+    EL_Q_b[i] += (var.arrival_rate_b[i] / arrival_rate_agg[i]) * EL_Q[i]
+
+EL_Q_l = np.zeros(var.n)
+for i in range(var.n):
+    EL_Q_l[i] += (var.arrival_rate_l[i] / arrival_rate_agg[i]) * EL_Q[i]
+
+EL_Q_f = np.zeros(var.n)
+for i in range(var.n):
+    EL_Q_f[i] += (var.arrival_rate_f[i] / arrival_rate_agg[i]) * EL_Q[i]
+
+print("EL_Q_b: \n", EL_Q_b)
+print("EL_Q_l: \n", EL_Q_l)
+print("EL_Q_f: \n", EL_Q_f)
+
+EL_b = EL_Q_b
+for i in range(var.n):
+    EL_b[i] += var.arrival_rate_b[i] * var.expected_service_time_b[i]
+
+EL_l = EL_Q_l
+for i in range(var.n):
+    EL_l[i] += var.arrival_rate_l[i] * var.expected_service_time_l[i]
+
+EL_f = EL_Q_f
+for i in range(var.n):
+    EL_f[i] += var.arrival_rate_f[i] * var.expected_service_time_f[i]
+
+print("EL_b: \n", EL_b)
+print("EL_l: \n", EL_l)
+print("EL_f: \n", EL_f)
+
+EW_b = 0
+for i in range(var.n):
+    if var.arrival_rate_b[i] == 0:
+        continue
+    EW_b += var.V_b[i] * (EL_b[i] / var.arrival_rate_b[i])
+
+EW_l = 0
+for i in range(var.n):
+    if var.arrival_rate_l[i] == 0:
+        continue
+    EW_l += var.V_l[i] * (EL_l[i] / var.arrival_rate_l[i])
+
+EW_f = 0
+for i in range(var.n):
+    if var.arrival_rate_f[i] == 0:
+        continue
+    EW_f += var.V_f[i] * (EL_f[i] / var.arrival_rate_f[i])
+
+print("EW_b : \n", EW_b)
+print("EW_l : \n", EW_l)
+print("EW_f : \n", EW_f)
---
+++ AQM4/variables_old.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+import numpy as np
+
+stations = ["0: ",  "1: SD", "2: Bb", "3: Bl", "4: Bf", "5: AGV", "6: ADBf", "7: A", "8: ABf", "9: D", "10: P", "11: Pa", "12: DBb", "13: DBl", "14: DBf"]
+O = 0
+SD = 1
+Bb = 2
+Bl = 3
+Bf = 4
+AGV = 5
+ADBf = 6
+A = 7
+ABf = 8
+D = 9
+P = 10
+Pa = 11
+DBb = 12
+DBl = 13
+DBf = 14
+
+n = len(stations)
+
+servers = [1, 3, np.inf, np.inf, np.inf, 1, np.inf, 2, np.inf, np.inf, 2, 1, np.inf, np.inf, np.inf]
+
+batch_size = 5
+
+lam_b = 6/60
+lam_l = 6/60
+lam_f = 3/60
+beta_b = lam_b/batch_size
+beta_l = lam_l/batch_size
+beta_f = lam_f/batch_size
+
+
+arrival_rate_b = np.array([beta_b, lam_b, lam_b, 0, 0, 2 * beta_b, 0, 0, 0, beta_b, beta_b, beta_b, beta_b, 0, 0])
+arrival_rate_l = np.array([beta_l, lam_l, 0, lam_l, 0, 2 * beta_l, 0, 0, 0, 2 * beta_l, 2 * beta_l, beta_l, 0, beta_l, 0])
+arrival_rate_f = np.array([beta_f, lam_f, 0, 0, lam_f, 2 * beta_f + beta_f, beta_f, lam_f, lam_f, beta_f + beta_f, beta_f + beta_f, 0, 0, 0, beta_f])
+
+departure_rate_b = np.array([beta_b, lam_b, beta_b, 0, 0, 2 * beta_b, 0, 0, 0, beta_b, beta_b, beta_b, beta_b, 0, 0])
+departure_rate_l = np.array([beta_l, lam_l, 0, beta_l, 0, 2 * beta_l, 0, 0, 0, 2 * beta_l, 2 * beta_l, beta_l, 0, beta_l, 0])
+departure_rate_f = np.array([beta_f, lam_f, 0, 0, beta_f, 2 * beta_f + beta_f, lam_f, lam_f, beta_f, beta_f + beta_f, beta_f + beta_f, 0, 0, 0, beta_f])
+
+expected_service_time_b = np.array([0, 8, (batch_size - 1) / (2*lam_b), 0, 0, 0, 0, 0, 0, 120, 18, 4, 0, 0, 0])
+expected_service_time_l = np.array([0, 8, 0, (batch_size - 1) / (2*lam_l), 0, 0, 0, 0, 0, 120, 18, 4, 0, 0, 0])
+expected_service_time_f = np.array([0, 8, 0, 0, (batch_size - 1) / (2*lam_f), 0, 0, 35, (batch_size - 1) / (2*lam_f), 120, 6, 0, 0, 0, 0])
+
+SCV_b = np.array([0, 3/2, 3/40, 0, 0, 0, 0, 0, 0, 0, 13/18, 2, 0, 0, 0])
+SCV_l = np.array([0, 3/2, 0, 3/40, 0, 0, 0, 0, 0, 0, 13/18, 2, 0, 0, 0])
+SCV_f = np.array([0, 3/2, 0, 0, 3/40, 0, 0, 1/2, 1/2, 0, 1/2, 0, 0, 0, 0])
+
+
+expected_service_time_AGV = 56/13
+SCV_AGV = 463/1568
+
+
+Pb = np.zeros([n,n])
+Pb[SD,Bb] += 1
+Pb[Bb, AGV] += 1
+Pb[AGV, P] += 0.5
+Pb[AGV,Pa] += 0.5
+Pb[P, D] += 1
+Pb[D, AGV] += 1
+Pb[Pa, O] += 1
+Pb[O, DBb] += 1
+Pb[DBb, SD] += 1
+
+Pl = np.zeros([n,n])
+Pl[SD,Bl] += 1
+Pl[Bl,AGV] += 1
+Pl[AGV, P] += 0.5
+Pl[AGV, Pa] += 0.5
+Pl[P, D] += 1
+Pl[D, P] += 0.5
+Pl[D, AGV] += 0.5
+Pl[Pa, O] += 1
+Pl[O, DBl] += 1
+Pl[DBl, SD] += 1
+
+Pf = np.zeros([n,n])
+Pf[SD, Bf] += 1
+Pf[Bf, AGV] += 1
+Pf[AGV, P] += (beta_f + beta_f) / (2 * beta_f + beta_f)
+Pf[AGV, ADBf] += (beta_f) / (2 * beta_f + beta_f)
+Pf[ADBf, A] += 1
+Pf[A, ABf] += 1
+Pf[ABf, AGV] += 1
+Pf[P, D] += 1
+Pf[D, AGV] += (beta_f) / (beta_f + beta_f)
+Pf[D, O] += (beta_f) / (beta_f + beta_f)
+Pf[O, DBf] += 1
+Pf[DBf, SD] += 1
